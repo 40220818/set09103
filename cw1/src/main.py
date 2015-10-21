670489dc14a99947411cf42efadd2b3fb9ca4731
Ruben Briones <40220818@live.napier.ac.uk>
2015-10-21 14:38:55 +0000

    New templates created and DB connection fully working

--- cw1/src/main.py
+++ cw1/src/main.py
@@ -1,26 +1,66 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
-from tmdb3 import set_key, searchMovie
+from tmdb3 import Movie, set_key, searchMovie, searchPerson, searchStudio
 set_key('8756c376f367fb966d3c0dec2c69e1c5')
 
 @app.route("/")
 def root():
   return render_template('base.html')
 
+@app.route("/noresults/")
+def noresults():
+  return render_template('no_results.html')
+
 @app.route("/movies/", methods=['POST', 'GET'])
 def movies():
   if request.method == 'POST':
     keyword = request.form['keyword']
     res = searchMovie(keyword)
-    return res[0].title
+    if len(res) == 0:
+      return redirect(url_for('noresults'))
+    else:
+      return res[0].title
   else:
     return render_template('movies.html')
 
-@app.route("/people/")
+@app.route("/people/", methods=['POST', 'GET'])
 def people():
-  return render_template('people.html')
+  if request.method == 'POST':
+    keyword = request.form['keyword']
+    res = searchPerson(keyword)
+    if len(res) == 0:
+      return redirect(url_for('noresults'))
+    else:
+      return res[0].name
+  else:
+    return render_template('people.html')
 
+@app.route("/studios/", methods=['POST', 'GET'])
+def studios():
+  if request.method == 'POST':
+    keyword = request.form['keyword']
+    res = searchStudio(keyword)
+    if len(res) == 0:
+      return redirect(url_for('noresults'))
+    else:
+      return res[0].name
+  else:
+    return render_template('studios.html')
+
+@app.route("/mostpopular/")
+def mostpopular():
+  movies = Movie.mostpopular()
+  res = movies[0:10]
+  ##res = ""
+  ##for m in movies[0:10]:
+  ##  res += m.title+'\n'
+  ##return res
+  return render_template('mostpopular.html', movies=res)
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('not_found.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
